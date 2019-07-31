429bfc4a5ce889d199db8c841e154f56a8c92179
Andrey Belyaev <belyaev.andrey@gmail.com>
2019-07-31 17:10:28 +0300

    Added class fix

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ unzip target/lmf-boot-prototype-0.0.1-SNAPSHOT.jar -d target/lmf-boot-prototype-
 echo CLASSPATH : ".:$(echo target/lmf-boot-prototype-0.0.1-SNAPSHOT/BOOT-INF/lib/*.jar | tr ' ' ':')":target/lmf-boot-prototype-0.0.1-SNAPSHOT/BOOT-INF/classes
 native-image  --no-server \
               --allow-incomplete-classpath \
+              --no-fallback \
+              --initialize-at-build-time=org.springframework.util.unit.DataSize
               -H:ReflectionConfigurationFiles=graal.json \
               -Dio.netty.noUnsafe=true \
               -H:+ReportUnsupportedElementsAtRuntime \
