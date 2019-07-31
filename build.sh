27fcda54cfdce37422a8194a994f8cb9f692f71f
Andrey Belyaev <belyaev.andrey@gmail.com>
2019-07-31 16:52:08 +0300

    Classpath fix

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 ./mvnw clean install
 pwd
 unzip target/lmf-boot-prototype-0.0.1-SNAPSHOT.jar -d target/lmf-boot-prototype-0.0.1-SNAPSHOT
-".:$(echo target/lmf-boot-prototype-SNAPSHOT/BOOT-INF/lib/*.jar | tr ' ' ':')"
+echo CLASSPATH : ".:$(echo target/lmf-boot-prototype-0.0.1-SNAPSHOT/BOOT-INF/lib/*.jar | tr ' ' ':')":target/lmf-boot-prototype-0.0.1-SNAPSHOT/BOOT-INF/classes
 native-image  --no-server \
               -H:ReflectionConfigurationFiles=graal.json \
               -Dio.netty.noUnsafe=true \
               -H:+ReportUnsupportedElementsAtRuntime \
               -Dfile.encoding=UTF-8 \
-              -cp ".:$(echo target/lmf-boot-prototype-SNAPSHOT/BOOT-INF/lib/*.jar | tr ' ' ':')":target/lmf-boot-prototype-0.0.1-SNAPSHOT/BOOT-INF/classes \
+              -cp ".:$(echo target/lmf-boot-prototype-0.0.1-SNAPSHOT/BOOT-INF/lib/*.jar | tr ' ' ':')":target/lmf-boot-prototype-0.0.1-SNAPSHOT/BOOT-INF/classes \
               com.haulmont.joker.demo.GraalDemoApplication
