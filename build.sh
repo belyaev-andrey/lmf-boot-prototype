4680a433310b0ab0588d1f52c5d019316f116ba8
Andrey Belyaev <belyaev.andrey@gmail.com>
2019-07-31 16:39:58 +0300

    Removed failing tests

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env sh
 ./mvnw clean install
+pwd
 unzip target/lmf-boot-prototype-0.0.1-SNAPSHOT.jar -d target/lmf-boot-prototype-0.0.1-SNAPSHOT
-native-image -H:ReflectionConfigurationFiles=graal.json -Dio.netty.noUnsafe=true -H:+ReportUnsupportedElementsAtRuntime -Dfile.encoding=UTF-8 -cp ".:$(echo target/lmf-boot-prototype-SNAPSHOT/BOOT-INF/lib/*.jar | tr ' ' ':')":target/lmf-boot-prototype-0.0.1-SNAPSHOT/BOOT-INF/classes com.haulmont.joker.demo.GraalDemoApplication
+".:$(echo target/lmf-boot-prototype-SNAPSHOT/BOOT-INF/lib/*.jar | tr ' ' ':')"
+native-image  --no-server \
+              -H:ReflectionConfigurationFiles=graal.json \
+              -Dio.netty.noUnsafe=true \
+              -H:+ReportUnsupportedElementsAtRuntime \
+              -Dfile.encoding=UTF-8 \
+              -cp ".:$(echo target/lmf-boot-prototype-SNAPSHOT/BOOT-INF/lib/*.jar | tr ' ' ':')":target/lmf-boot-prototype-0.0.1-SNAPSHOT/BOOT-INF/classes \
+              com.haulmont.joker.demo.GraalDemoApplication
